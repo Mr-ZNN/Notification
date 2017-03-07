b6c43aad541c6e1cd1694d081d8eb4ac5838851e
张宁宁 <1511314251@qq.com>
2017-03-07 16:07:23 +0800

    初始化了一个项目

---
+++ Test/Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by 张宁宁 on 17/3/7.
+//  Copyright © 2017年 all. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Test/Test/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  Test
+//
+//  Created by 张宁宁 on 17/3/7.
+//  Copyright © 2017年 all. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"来个测试");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
